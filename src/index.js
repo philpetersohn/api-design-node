d4c92402290ed12a76f67cf7793c0f3fdcc78f6b
philpetersohn <petersohn.phil@gmail.com>
2024-12-17 18:08:45 -0800

    Created express server

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-const http = require("http");
+const app = require("./server");
 
-const server = http.createServer((req, res) => {
-  if (req.method === "GET" && req.url === "/") {
-    res.statusCode = 200;
-    console.log("Hello from the server");
-    res.end();
-  }
-});
-
-server.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(3000, () => {
+  console.log("Hello on http://localhost:3000");
 });
